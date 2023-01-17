176cccc53a8a388b0cf7e942f08dc767762213dd
Vinícios Oliveira <vinicompif@gmail.com>
2023-01-17 18:39:47 -0300

    lesson 01

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { Habit } from "./components/Habit";
+
+function App() {
+  return (
+    <>
+      <Habit completed={10}></Habit>
+      <Habit completed={10}></Habit>
+      <Habit completed={10}></Habit>
+      <Habit completed={10}></Habit>
+      <Habit completed={10}></Habit>
+    </>
+  );
+}
+
+export default App;
