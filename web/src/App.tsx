2c82b6f6e31ecaed8733a13beb6a8c5b8f06ffb2
Vinícios Oliveira <vinicompif@gmail.com>
2023-01-21 17:00:51 -0300

    lesson 02 frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,16 @@
-import { Habit } from "./components/Habit";
+// import { Habit } from "./components/Habit";
+import logoImg from "./assets/logo.svg";
+import { Plus } from "phosphor-react";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
-function App() {
+export function App() {
   return (
-    <>
-      <Habit completed={10}></Habit>
-      <Habit completed={10}></Habit>
-      <Habit completed={10}></Habit>
-      <Habit completed={10}></Habit>
-      <Habit completed={10}></Habit>
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16 ">
+        <Header></Header>
+        <SummaryTable></SummaryTable>
+      </div>
+    </div>
   );
 }
-
-export default App;
